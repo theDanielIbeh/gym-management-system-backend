bfc1772ce7340847e01a4b28662f660e5444b4df
William Pepple <williampepple1@gmail.com>
2024-01-05 11:42:26 +0100

    Update payments.model.ts

--- src/models/payments.model.ts
+++ src/models/payments.model.ts
@@ -2,15 +2,15 @@ import { model, Schema, Document, Types } from 'mongoose';
 import { Payment } from '@/interfaces/payments.interface';
 
 export const PaymentType = {
-  Subscription: "Subscription",
-  Salary: "Salary",
-  Utility: "Utility",
-}
+  Subscription: 'Subscription',
+  Salary: 'Salary',
+  Utility: 'Utility',
+};
 const PaymentSchema: Schema = new Schema({
   payment_type: {
     type: String,
     required: true,
-    enum: Object.values(PaymentType)
+    enum: Object.values(PaymentType),
   },
   amount: {
     type: Number,
@@ -19,12 +19,13 @@ const PaymentSchema: Schema = new Schema({
   date: {
     type: Date,
     required: true,
+    default: Date.now,
   },
   payee: {
     type: Types.ObjectId,
-    ref: "User",
-    required: true
-  }
+    ref: 'User',
+    required: true,
+  },
 });
 
 export const PaymentModel = model<Payment & Document>('Payment', PaymentSchema);
