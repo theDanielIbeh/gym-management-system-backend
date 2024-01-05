18c36f750be3166379c474652b0590b4a0c275f1
William Pepple <williampepple1@gmail.com>
2024-01-05 02:19:40 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import { App } from '@/app';
 import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
+import { BranchRoute } from './routes/branches.route';
 
 ValidateEnv();
 
-const app = new App([new UserRoute(), new AuthRoute()]);
+const app = new App([new UserRoute(), new AuthRoute(), new BranchRoute()]);
 
 app.listen();
