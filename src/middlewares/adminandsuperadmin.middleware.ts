deefab8ca464ec1173d90abb71afa15c457799fb
William Pepple <williampepple1@gmail.com>
2024-01-05 12:39:46 +0100

    Create adminandsuperadmin.middleware.ts

---
+++ src/middlewares/adminandsuperadmin.middleware.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import { NextFunction, Response } from 'express';
+import { SECRET_KEY } from '@config';
+import { RequestWithUser } from '@interfaces/auth.interface';
+
+interface MyTokenPayload {
+  role: string;
+}
+
+export const checkRole = (...roles) => {
+  return (req: RequestWithUser, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      return res.status(403).json({ message: 'Access denied, no token provided.' });
+    }
+
+    try {
+      const decoded = jwt.verify(token, SECRET_KEY) as MyTokenPayload;
+      const userRole = decoded.role;
+
+      if (!roles.includes(userRole)) {
+        return res.status(403).json({ message: 'Access denied, insufficient permissions.' });
+      }
+
+      next();
+    } catch (error) {
+      res.status(400).json({ message: 'Invalid token.' });
+    }
+  };
+};
