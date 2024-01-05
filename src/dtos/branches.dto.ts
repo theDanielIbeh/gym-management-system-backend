d7306adbb09b1fdb9fb41730baa6fd7d77f64d09
William Pepple <williampepple1@gmail.com>
2024-01-05 02:05:11 +0100

    Create branches.dto.ts

---
+++ src/dtos/branches.dto.ts
@@ -0,0 +1,38 @@
+import { IsString, IsNotEmpty, IsOptional, IsNumber, IsArray } from 'class-validator';
+
+export class CreateBranchDto {
+  @IsString()
+  @IsNotEmpty()
+  public name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public location: string;
+
+  @IsString()
+  @IsOptional()
+  public branch: String;
+
+  @IsNumber()
+  @IsOptional()
+  utility_cost: number;
+
+  @IsString()
+  @IsOptional()
+  public admin: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  public users: string[];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  public staff: string[];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  public equipment: string[];
+}
