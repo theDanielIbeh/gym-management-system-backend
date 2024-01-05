a5f915cc165ba1704a9dfe5966ad51515e9e10e1
theDanielIbeh <dibeh1000@gmail.com>
2024-01-05 10:28:06 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
 import { BranchRoute } from './routes/branches.route';
+import { PaymentRoute } from './routes/payments.route';
 
 ValidateEnv();
 
-const app = new App([new UserRoute(), new AuthRoute(), new BranchRoute()]);
+const app = new App([new UserRoute(), new AuthRoute(), new BranchRoute(), new PaymentRoute()]);
 
 app.listen();
