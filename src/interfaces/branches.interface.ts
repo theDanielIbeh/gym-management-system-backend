01e62389273386e487f49e0e70efdb5d974c63d5
William Pepple <williampepple1@gmail.com>
2024-01-05 01:50:47 +0100

    Update branches.interface.ts
    Add User types to the branch interface

--- src/interfaces/branches.interface.ts
+++ src/interfaces/branches.interface.ts
@@ -1,10 +1,12 @@
+import { User } from './users.interface';
+
 export interface Branch {
   _id?: string;
   name: string;
   location: string;
-  admin: string;
-  users: string[];
+  admin: User;
+  users: User[];
   equipment: string[];
   utility_cost: number;
-  staff: string[];
+  staff: User[];
 }
