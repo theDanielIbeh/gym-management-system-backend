e54333ccd6bd070ff58da778bd3d2cb0938b75c5
William Pepple <williampepple1@gmail.com>
2024-01-05 12:40:15 +0100

    Update payments.route.ts

--- src/routes/payments.route.ts
+++ src/routes/payments.route.ts
@@ -4,6 +4,7 @@ import { CreatePaymentDto } from '@dtos/payments.dto';
 import { Routes } from '@interfaces/routes.interface';
 import { ValidationMiddleware } from '@middlewares/validation.middleware';
 import { checkSuperAdminRole } from '@/middlewares/superadmin.middleware';
+import { checkRole } from '@/middlewares/adminandsuperadmin.middleware';
 
 export class PaymentRoute implements Routes {
   public path = '/payment';
@@ -15,7 +16,7 @@ export class PaymentRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.payment.getPayments);
+    this.router.get(`${this.path}`, checkRole('Superadmin', 'Admin'), this.payment.getPayments);
     this.router.get(`${this.path}/:id`, this.payment.getPaymentById);
     this.router.post(`${this.path}`, checkSuperAdminRole('Superadmin'), ValidationMiddleware(CreatePaymentDto), this.payment.createPayment);
     this.router.delete(`${this.path}/:id`, checkSuperAdminRole('Superadmin'), this.payment.deletePayment);
