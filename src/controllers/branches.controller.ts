b0d22e5aece4fc3d789852518c6c3fc8e875a169
William Pepple <williampepple1@gmail.com>
2024-01-10 17:14:33 +0100

    Update branches.controller.ts

--- src/controllers/branches.controller.ts
+++ src/controllers/branches.controller.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { Container } from 'typedi';
 import { Branch } from '@interfaces/branches.interface';
 import { BranchService } from '@services/branches.service';
+import { Admin } from '@/interfaces/users.interface';
+import { Types } from 'mongoose';
+import { AssignBranchDto } from '@/dtos/branches.dto';
 
 export class BranchController {
   public branch = Container.get(BranchService);
@@ -43,6 +46,20 @@ export class BranchController {
       const branchId: string = req.params.id;
       const branchData: Branch = req.body;
       const updateBranchData: Branch = await this.branch.updateBranch(branchId, branchData);
+
+      res.status(200).json({ data: updateBranchData, message: 'updated' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public updateBranchWithAdmin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const branchId: string = req.params.id;
+      const assignBranchDto: AssignBranchDto = req.body;
+      const adminId: string = assignBranchDto.admin;
+
+      const updateBranchData: Branch = await this.branch.assignAdminToBranch(branchId, adminId);
 
       res.status(200).json({ data: updateBranchData, message: 'updated' });
     } catch (error) {
