bc9d43cb0b992b25d0de1d93140b627ae8c73cae
William Pepple <williampepple1@gmail.com>
2024-01-10 17:20:38 +0100

    Update Branch route

--- src/routes/branches.route.ts
+++ src/routes/branches.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { BranchController } from '@controllers/branches.controller';
-import { CreateBranchDto } from '@dtos/branches.dto';
+import { AssignBranchDto, CreateBranchDto } from '@dtos/branches.dto';
 import { Routes } from '@interfaces/routes.interface';
 import { ValidationMiddleware } from '@middlewares/validation.middleware';
 import { checkSuperAdminRole } from '@/middlewares/superadmin.middleware';
@@ -18,6 +18,12 @@ export class BranchRoute implements Routes {
     this.router.get(`${this.path}`, checkSuperAdminRole('Superadmin'), this.branch.getBranches);
     this.router.get(`${this.path}/:id`, checkSuperAdminRole('Superadmin'), this.branch.getBranchById);
     this.router.post(`${this.path}`, checkSuperAdminRole('Superadmin'), ValidationMiddleware(CreateBranchDto), this.branch.createBranch);
+    this.router.put(
+      `${this.path}/assign/:id`,
+      checkSuperAdminRole('Superadmin'),
+      ValidationMiddleware(AssignBranchDto),
+      this.branch.updateBranchWithAdmin,
+    );
     this.router.put(`${this.path}/:id`, checkSuperAdminRole('Superadmin'), ValidationMiddleware(CreateBranchDto, true), this.branch.updateBranch);
     this.router.delete(`${this.path}/:id`, checkSuperAdminRole('Superadmin'), this.branch.deleteBranch);
   }
