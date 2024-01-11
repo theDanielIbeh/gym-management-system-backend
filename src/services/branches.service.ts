e37312df4fa921d7028820f73e70e97630292d84
theDanielIbeh <dibeh1000@gmail.com>
2024-01-11 12:57:53 +0000

    Update branches.service.ts
    Added the addUserToBranch function

--- src/services/branches.service.ts
+++ src/services/branches.service.ts
@@ -7,12 +7,12 @@ import { UserModel } from '@/models/users.model';
 @Service()
 export class BranchService {
   public async findAllBranches(): Promise<Branch[]> {
-    const branchs: Branch[] = await BranchModel.find().populate('admin').populate('staff');
+    const branchs: Branch[] = await BranchModel.find().populate('admin').populate('staff').populate('users');
     return branchs;
   }
 
   public async findBranchById(branchId: string): Promise<Branch> {
-    const findBranch: Branch = await BranchModel.findOne({ _id: branchId }).populate('admin');
+    const findBranch: Branch = (await BranchModel.findOne({ _id: branchId }).populate('admin').populate('users'));
     if (!findBranch) throw new HttpException(409, "Branch doesn't exist");
 
     return findBranch;
@@ -76,6 +76,29 @@ export class BranchService {
       { $addToSet: { staff: userId } }, // This adds the staff member without duplicating if they're already in the array
       { new: true },
     ).populate('staff');
+
+    if (!updatedBranch) {
+      throw new HttpException(409, "Branch doesn't exist");
+    }
+
+    return updatedBranch;
+  }
+
+  public async addUserToBranch(branchId: string, userId: string): Promise<Branch> {
+    // Check if the user exists (and optionally check for appropriate role)
+    const user = await UserModel.findById(userId /*, { role: 'user' } */);
+    if (!user) {
+      throw new HttpException(409, 'User does not exist');
+    }
+    if (user.role !== 'User') {
+      throw new HttpException(409, 'User is not a User Member');
+    }
+    // Append the user to the staff array of the branch
+    const updatedBranch: Branch = await BranchModel.findByIdAndUpdate(
+      branchId,
+      { $addToSet: { users: userId } }, // This adds the user member without duplicating if they're already in the array
+      { new: true },
+    ).populate('users');
 
     if (!updatedBranch) {
       throw new HttpException(409, "Branch doesn't exist");
